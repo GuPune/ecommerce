d9c8a573ec485362b7d9a79efb80e8fbabbd2776
boonkhet idklever <iboonkhet@gmail.com>
2022-01-30 16:01:54 +0700

    update

---
+++ plugins/gtag.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import VueGtag from 'vue-gtag'
+import axios from 'axios'
+
+let a = window.location.origin;
+
+axios.post('https://cmsecom.idtest.work/api/gtag', {
+    data: a
+  }).then(res => {
+ Vue.use(VueGtag, {
+    config: { id: res.data }
+  })
+  }).catch(function(){
+     
+  
+  
+});
+
+
